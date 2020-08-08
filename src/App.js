8f3a3d3c0136394d3daf39215396c7c22f37715f
Dumilde Matos <dumilde.matos@jupiter.co.ao>
2020-08-08 20:03:56 +0100

    Creating Portfolio Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/home/Home'; 
+import Menu from './components/menu/Menu';
 import Portfolio from './components/portfolio/Portfolio'
 
 function App() {
   return (
     <Router>
       <Switch>
+        
         <Route path="/" exact component={Home}></Route>
         <Route path="/portfolio" component={Portfolio}></Route>
         <Route path="/" component={Home}></Route>
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -3,17 +3,58 @@ import Bounce from 'react-reveal/Bounce';
 import './Portfolio.css';
 import logo from '../../logo.svg';
 import Menu from '../menu/Menu';
+import $ from 'jquery';
+import ReactDOM from 'react-dom';
+
 function Portfolio(){
+
+  // console.log(document.getElementsByClassName('thumb'))
   document.title = 'Portfolio :: Dumilde Matos';
+    
   return(
-    <div className="container-box flex">
+    <div className="container-box flex flex-rows portfolio">
       <div className="row flex center height-70p">
-        <Menu/>  
+        <Menu />
         <div className="logo-box-nav">
           <img src={logo} alt="logo" />
         </div>
       </div>
-    </div>
-  );
+      <div className="row main-content">
+        <div className="card thumb">
+          <span>UI/UX</span>
+        </div>        
+        <div className="card thumb">
+          <span>Web Development</span>
+        </div>        
+        <div className="card thumb">
+          <span>Mobile Development</span>
+        </div>
+      </div>
+      </div>
+      );
+  
 }
+
+if(document.location.href.includes('/portfolio')){
+  console.log('here!')
+  // $(function() {
+  //   document.location.reload();
+  //   $('.card').each(function(i){
+  //     setTimeout(function(){
+  //       $('.card').eq(i).addClass('is-visible');
+  //     }, 1000 * i)
+  //   })
+  // });
+  Portfolio();
+  $(".portfolio").ready(function() {
+    console.log($('.card'))
+    $('.card').each(function(i){
+      setTimeout(function(){
+        $('.card').eq(i).addClass('is-visible');
+      }, 1000 * i)
+    })
+  });
+}
+
+
 export default Portfolio;
